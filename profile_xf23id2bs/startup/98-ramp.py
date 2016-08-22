5dbf94c50370d93f3cd432b03f1a3cb2d1b0b530
CSX-2 Operator <xf23id2@xf23id-ws1.cs.nsls2.local>
2016-08-22 14:06:18 -0400

    MNT: clean up ramp implementation
     - use upstream version of ramp_plan
     - make sure gap does not run back to start position when
       the interpolator source is reset

--- profile_xf23id2bs/startup/98-ramp.py
+++ profile_xf23id2bs/startup/98-ramp.py
@@ -3,83 +3,6 @@ from ophyd import StatusBase
 import time
 from bluesky.spec_api import inner_spec_decorator, setup_plot, setup_livetable
 
-def ramp_plan(configuration_plan,
-              go_plan,
-              monitor_sig,
-              inner_plan_func,
-              timeout=None,
-              period=None, md=None):
-    '''Take data while ramping one or more positioners.
-
-    The pseudo code for this plan is ::
-
-       yield from configure_plan
-       sts = (yield from open_run())
-       yield from inner_plan_func()
-       while not st.don:
-           yield from inner_plan_func()
-       yield from inner_plan_func()
-       yield from close_run()
-    Parameters
-    ----------
-    configuration_plan : iterable
-        plan to set the system
-    go_plan : iterable
-        plan to start the ramp.  This will be run inside of a open/close
-        run.
-        This plan must return a status object.
-    inner_plan_func : generator function
-        generator which takes no input
-        This will be called for every data point.  This should create
-        one or more events.
-    timeout : float, optional
-        If not None, the maximum time the ramp can run.
-        In seconds
-    period : fload, optional
-        If not None, take data no faster than this.  If None, take
-        data as fast as possible
-        If running the inner plan takes longer than `period` than take
-        data with no dead time.
-        In seconds.
-    '''
-    if md is None:
-        md = {}
-
-    @monitor_during_decorator((monitor_sig,))
-    @run_decorator(md=md)
-    def polling_plan():
-        # sort out if we should watch the clock
-        fail_time = None
-        last_time = None
-        if timeout is not None:
-            fail_time = time.time() + timeout
-        if period:
-            last_time = time.time()
-
-        
-        # take a 'pre' data point
-        yield from inner_plan_func()
-        # start the ramp
-        status = (yield from go_plan)
-
-        while not status.done:
-            yield from inner_plan_func()
-            if fail_time is not None:
-                if time.time() > fail_time:
-                    raise RampFail()
-            if period is not None:
-                cur_time = time.time()
-                wait_time = (last_time + period) - cur_time
-                last_time = cur_time
-                if wait_time > 0:
-                    yield from sleep(wait_time)
-
-        # take a 'post' data point
-        yield from inner_plan_func()
-
-    return (yield from configuration_plan(polling_plan()))
-
-
 def change_epu_flt_link(new_target):
     v = (yield from read(epu1.flt.input_pv))
     if v is None:
@@ -89,9 +12,16 @@ def change_epu_flt_link(new_target):
     pts = cur_pv.split(' ', maxsplit=1)
     new_pv = ' '.join([new_target] + pts[1:])
     yield from abs_set(epu1.flt.input_pv, new_pv)
-    
 
-def conf_plan(polling_plan):
+    
+def _run_E_ramp(dets, start, stop, velocity, *, md=None):
+    # put the energy at the starting value
+    yield from abs_set(pgm.energy, start, wait=True)
+
+    yield from abs_set(pgm.fly.start, start, wait=True)
+    yield from abs_set(pgm.fly.stop, stop, wait=True)
+    yield from abs_set(pgm.fly.velocity, velocity, wait=True)
+
     # get the old vlaue
     v = (yield from read(epu1.flt.input_pv))
     if v is None:
@@ -102,24 +32,14 @@ def conf_plan(polling_plan):
 
     # define a clean up plan
     def clean_up():
-        yield from abs_set(pgm.energy, stop, wait=True)
+        # move the energy setpoint to where the energy really is
+        yield from abs_set(pgm.energy, pgm.energy.position, wait=True)
+        # set the interpolator to look at what it was looking at before
+        # the scan.  This should be the energy set point.
         yield from abs_set(epu1.flt.input_pv, old_link)
-
     
     # change to track the readout energy
     yield from change_epu_flt_link(pgm_energy.readback.pvname)
-    
-    return (yield from finalize_wrapper(polling_plan, clean_up()))
-
-
-def _run_E_ramp(dets, start, stop, velocity, *, md=None):
-    # put the energy at the starting value
-    yield from abs_set(pgm.energy, start, wait=True)
-
-    yield from abs_set(pgm.fly.start, start, wait=True)
-    yield from abs_set(pgm.fly.stop, stop, wait=True)
-    yield from abs_set(pgm.fly.velocity, velocity, wait=True)
-
     
     def go_plan():
         ret = (yield from abs_set(pgm.fly.fly_start, 1))
@@ -144,9 +64,11 @@ def _run_E_ramp(dets, start, stop, velocity, *, md=None):
     def inner_plan():
         yield from trigger_and_read(dets)
 
-    ret = (yield from ramp_plan(conf_plan, go_plan(), pgm.energy,
-                                inner_plan, period=None, md=md))
-    return ret
+    rp = ramp_plan(go_plan(), pgm.energy,
+                   inner_plan, period=None, md=md)
+        
+    return (yield from finalize_wrapper(rp, clean_up()))
+
     
 def E_ramp(start, stop, velocity, time=None, *, md=None):
     motors = [pgm.energy]
@@ -155,3 +77,16 @@ def E_ramp(start, stop, velocity, time=None, *, md=None):
     return (yield from inner(gs.DETS + [pgm.energy], start, stop, velocity, md=md))
 
 gs.SUB_FACTORIES['E_ramp'] = [setup_plot, setup_livetable]
+
+
+def _epu_ramp(dets, start, stop):
+    def go_plan():
+        return (yield from abs_set(epu1.gap, stop, wait=False))
+
+    def inner_plan():
+        yield from trigger_and_read(dets)
+
+    yield from abs_set(epu1.gap, start, wait=True)
+        
+    return (yield from (ramp_plan(go_plan(), pgm.energy,
+                                  inner_plan, period=None, md=md)))
