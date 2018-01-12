513d6d53772a32970948b65e75ae2c53cc0eddc0
irawaluyo <iwaluyo@bnl.gov>
2018-01-12 09:59:57 -0500

    rename signal_names to component_names

--- profile_xf23id2bs/startup/20-detectors.py
+++ profile_xf23id2bs/startup/20-detectors.py
@@ -4,7 +4,7 @@ from ophyd import EpicsScaler
 # CSX-2 Scalar
 
 sclr = EpicsScaler('XF:23ID2-ES{Sclr:1}', name='sclr')
-for sig in sclr.channels.signal_names:
+for sig in sclr.channels.component_names:
     getattr(sclr.channels, sig).name = 'sclr_' + sig.replace('an', '')
 sclr.channels.read_attrs = ['chan2', 'chan3', 'chan4']
 
